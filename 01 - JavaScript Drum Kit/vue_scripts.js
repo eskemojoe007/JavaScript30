337e7868fd070ca119497ce6e5ca03d8c9c607e6
Folkner, David Erik <david.folkner@ge.com>
2018-05-30 16:27:49 -0400

    Beautify

--- 01 - JavaScript Drum Kit/vue_scripts.js
+++ 01 - JavaScript Drum Kit/vue_scripts.js
@@ -1,13 +1,12 @@
-
 var soundDiv = {
   props: ['item'],
-  data: function () {
+  data: function() {
     return {
       isActive: false,
       show: true
     }
   },
-  mounted: function () {
+  mounted: function() {
     this.$el.addEventListener('transitionend', (e) => this.removeTransition(e));
   },
 
@@ -20,37 +19,71 @@ var soundDiv = {
   `,
 
   methods: {
-    play_sound: function (event) {
+    play_sound: function(event) {
       this.isActive = true
       const audio = this.$el.querySelector('audio');
       audio.currentTime = 0; //rewind to start
       audio.play();
     },
-    removeTransition: function (e) {
+    removeTransition: function(e) {
       if (e.propertyName !== 'transform') return; //only do stuff on transform
       this.isActive = false;
     }
   },
 }
-Vue.component('audio-file', {
+var audioFile = {
   props: ['item'],
   template: `
     <audio :src="'sounds/' + item.sound + '.wav'"></audio>
   `
 })
 
-var keysList =  [
-    {keyCode: "65", sound: "clap", kbd: "A"},
-    {keyCode: "83", sound: "hihat", kbd: "S"},
-    {keyCode: "68", sound: "kick", kbd: "D"},
-    {keyCode: "70", sound: "openhat", kbd: "F"},
-    {keyCode: "71", sound: "boom", kbd: "G"},
-    {keyCode: "72", sound: "ride", kbd: "H"},
-    {keyCode: "74", sound: "snare", kbd: "J"},
-    {keyCode: "75", sound: "tom", kbd: "K"},
-    {keyCode: "76", sound: "tink", kbd: "L"},
-  ]
-
+var keysList = [{
+    keyCode: "65",
+    sound: "clap",
+    kbd: "A"
+  },
+  {
+    keyCode: "83",
+    sound: "hihat",
+    kbd: "S"
+  },
+  {
+    keyCode: "68",
+    sound: "kick",
+    kbd: "D"
+  },
+  {
+    keyCode: "70",
+    sound: "openhat",
+    kbd: "F"
+  },
+  {
+    keyCode: "71",
+    sound: "boom",
+    kbd: "G"
+  },
+  {
+    keyCode: "72",
+    sound: "ride",
+    kbd: "H"
+  },
+  {
+    keyCode: "74",
+    sound: "snare",
+    kbd: "J"
+  },
+  {
+    keyCode: "75",
+    sound: "tom",
+    kbd: "K"
+  },
+  {
+    keyCode: "76",
+    sound: "tink",
+    kbd: "L"
+  },
+]
 var key_divs = new Vue({
   el: '#key-divs',
   data: {
@@ -58,14 +91,17 @@ var key_divs = new Vue({
   },
   components: {
     'sound-div': soundDiv,
+    'audio-file': audioFile,
   },
-  methods:{
-    keystroke: function (keyCode) {
-      const child = this.$children.find(child => {return child.$vnode.data.key === keyCode.toString()});
+  methods: {
+    keystroke: function(keyCode) {
+      const child = this.$children.find(child => {
+        return child.$vnode.data.key === keyCode.toString()
+      });
       child.play_sound()
     }
   },
-  created: function () {
+  created: function() {
     window.addEventListener('keydown', (e) => this.keystroke(e.keyCode));
   }
 })
