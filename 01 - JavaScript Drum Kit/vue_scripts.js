6d9a3c4a22cf0c31afdcf4b17fe51d7fb6dbf35a
Folkner, David Erik <david.folkner@ge.com>
2018-05-30 15:25:19 -0400

    Fully working vue 01

--- 01 - JavaScript Drum Kit/vue_scripts.js
+++ 01 - JavaScript Drum Kit/vue_scripts.js
@@ -1,31 +1,71 @@
-Vue.component('sound-div', {
+
+var soundDiv = {
   props: ['item'],
+  data: function () {
+    return {
+      isActive: false,
+      show: true
+    }
+  },
+  mounted: function () {
+    this.$el.addEventListener('transitionend', (e) => this.removeTransition(e));
+  },
+
   template: `
-      <div :data-key="item.keyCode" class="key" :onclick="'play(' + item.keyCode + ')'">
-        <kbd>{{ item.kbd }}</kbd>
-        <span class="sound">{{ item.sound }}</span>
-      </div>
-  `
-})
+    <div class="key" :class="{playing: isActive}" v-on:click='play_sound'>
+      <kbd class="noselect">{{ item.kbd }}</kbd>
+      <span class="sound noselect">{{ item.sound }}</span>
+      <audio-file :item="item"></audio-file>
+    </div>
+  `,
+
+  methods: {
+    play_sound: function (event) {
+      this.isActive = true
+      const audio = this.$el.querySelector('audio');
+      audio.currentTime = 0; //rewind to start
+      audio.play();
+    },
+    removeTransition: function (e) {
+      if (e.propertyName !== 'transform') return; //only do stuff on transform
+      this.isActive = false;
+    }
+  },
+}
 Vue.component('audio-file', {
   props: ['item'],
   template: `
-    <audio :data-key="item.keyCode" :src="'sounds/' + item.sound + '.wav'"></audio>
+    <audio :src="'sounds/' + item.sound + '.wav'"></audio>
   `
 })
 
-var app = new Vue({
-  el: '#app',
+var keysList =  [
+    {keyCode: "65", sound: "clap", kbd: "A"},
+    {keyCode: "83", sound: "hihat", kbd: "S"},
+    {keyCode: "68", sound: "kick", kbd: "D"},
+    {keyCode: "70", sound: "openhat", kbd: "F"},
+    {keyCode: "71", sound: "boom", kbd: "G"},
+    {keyCode: "72", sound: "ride", kbd: "H"},
+    {keyCode: "74", sound: "snare", kbd: "J"},
+    {keyCode: "75", sound: "tom", kbd: "K"},
+    {keyCode: "76", sound: "tink", kbd: "L"},
+  ]
+
+var key_divs = new Vue({
+  el: '#key-divs',
   data: {
-    keysList: [{keyCode: "65", sound: "clap", kbd: "A"},
-      {keyCode: "83", sound: "hihat", kbd: "S"},
-      {keyCode: "68", sound: "kick", kbd: "D"},
-      {keyCode: "70", sound: "openhat", kbd: "F"},
-      {keyCode: "71", sound: "boom", kbd: "G"},
-      {keyCode: "72", sound: "ride", kbd: "H"},
-      {keyCode: "74", sound: "snare", kbd: "J"},
-      {keyCode: "75", sound: "tom", kbd: "K"},
-      {keyCode: "76", sound: "tink", kbd: "L"},
-    ]
+    keysList: keysList,
+  },
+  components: {
+    'sound-div': soundDiv,
+  },
+  methods:{
+    keystroke: function (keyCode) {
+      const child = this.$children.find(child => {return child.$vnode.data.key === keyCode.toString()});
+      child.play_sound()
+    }
+  },
+  created: function () {
+    window.addEventListener('keydown', (e) => this.keystroke(e.keyCode));
   }
 })
